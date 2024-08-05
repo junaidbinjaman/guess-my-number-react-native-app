6b4d7c4e4ab8671b9d804d39816bc7beea88a6d2
junaidbinjaman <me@junaidbinjaman.com>
2024-08-05 10:00:00 +0600

    The custom font is added

--- components/ui/Tittle.js
+++ components/ui/Tittle.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { StyleSheet, Text } from 'react-native';
-import Colors from '../../constants/colors';
 
 const Tittle = ({children}) => {
     return (
@@ -12,8 +11,8 @@ export default Tittle;
 
 const styles = StyleSheet.create({
     title: {
+        fontFamily: 'open-sans-bold',
         fontSize: 24,
-        fontWeight: 'bold',
         color: 'white',
         textAlign: 'center',
         borderWidth: 2,
